f5557df86d875406f68edcf23929c52a04cd0647
mayankdutta <smayankdutt@gmail.com>
2022-04-10 11:56:45 +0530

    finishing remains

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./component/Home";
+import GameArea from "./component/GameArea";
+
+const App = () => {
+  return (
+    <div>
+      <GameArea></GameArea>
+    </div>
+  );
+};
+
+export default App;
